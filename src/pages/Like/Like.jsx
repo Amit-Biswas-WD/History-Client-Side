55b5f836320b1bf3abd20bc410669b16f5f055cc
Amit biswas <amitbiswas9282@gmail.com>
2025-03-06 23:09:58 +0600

    like

---
+++ src/pages/Like/Like.jsx
@@ -0,0 +1,36 @@
+const Like = () => {
+  return (
+    <div className="container mx-auto mt-24 mb-4 grid md:grid-cols-2 sm:grid-cols-1 gap-8">
+      <div className="col-span-1 border border-gray-300 shadow-2xl rounded-lg p-8">
+        <img
+          src="https://th-thumbnailer.cdn-si-edu.com/wsQUWrO5N2HJSvn6Dow8omdoQbQ=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/a6/60/a660065e-a460-48f3-b1ad-21b19fb7d1bb/firstchristmascard.jpg"
+          alt=""
+        />
+        <div className="my-6">
+          <h2 className="text-2xl font-semibold my-2">Name: </h2>
+          <p className="text-base font-normal my-2">Artifact Type: </p>
+          <p className="text-base font-normal my-2">Historical Context: </p>
+          <p className="text-base font-normal my-2">Created At: </p>
+          <p className="text-base font-normal my-2">Discovered At: </p>
+          <p className="text-base font-normal my-2">Location: </p>
+        </div>
+      </div>
+      <div className="col-span-1 border border-gray-300 shadow-2xl rounded-lg p-8">
+        <img
+          src="https://th-thumbnailer.cdn-si-edu.com/wsQUWrO5N2HJSvn6Dow8omdoQbQ=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/a6/60/a660065e-a460-48f3-b1ad-21b19fb7d1bb/firstchristmascard.jpg"
+          alt=""
+        />
+        <div className="my-6">
+          <h2 className="text-2xl font-semibold my-2">Name: </h2>
+          <p className="text-base font-normal my-2">Artifact Type: </p>
+          <p className="text-base font-normal my-2">Historical Context: </p>
+          <p className="text-base font-normal my-2">Created At: </p>
+          <p className="text-base font-normal my-2">Discovered At: </p>
+          <p className="text-base font-normal my-2">Location: </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Like;
