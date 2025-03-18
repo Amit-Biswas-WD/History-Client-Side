6bd26b93733bdcdcecbbbf32e9c965576d6dc999
Amit biswas <amitbiswas9282@gmail.com>
2025-03-19 00:16:11 +0600

    line

--- src/pages/Like/Like.jsx
+++ src/pages/Like/Like.jsx
@@ -3,18 +3,22 @@ import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const Like = () => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const [addData, setAddData] = useState([]);
   const axiosInstance = useAxiosSecure();
 
-  const url = `/like?email=${user?.email}`;
-
   useEffect(() => {
-    axiosInstance.get(url).then((res) => {
-      console.log(res.data);
-      setAddData(res.data);
-    });
-  }, [url, axiosInstance]);
+    if (!loading && user?.email) {
+      const url = `/like?email=${user.email}`;
+
+      axiosInstance.get(url)
+        .then((res) => {
+          console.log(res.data);
+          setAddData(res.data);
+        })
+        .catch((err) => console.error("Error fetching data:", err));
+    }
+  }, [user, loading, axiosInstance]);
 
   return (
     <div className="container mx-auto mt-30 mb-10 grid md:grid-cols-2 sm:grid-cols-1 gap-8">
@@ -23,8 +27,14 @@ const Like = () => {
           key={data._id}
           className="col-span-1 border border-gray-300 shadow-2xl rounded-lg p-8"
         >
-          <img src={data.photo} alt="Image not found" className="w-full h-[400px]" />
-          <h2 className="text-3xl font-semibold mb-4 mt-8">Name: {data.name}</h2>
+          <img
+            src={data.photo}
+            alt="Image not found"
+            className="w-full h-[400px]"
+          />
+          <h2 className="text-3xl font-semibold mb-4 mt-8">
+            Name: {data.name}
+          </h2>
           <div className="text-lg font-medium space-y-2">
             <p>
               Created At: <span className="font-normal">{data.createdAt}</span>
@@ -36,8 +46,7 @@ const Like = () => {
               Artifact Type: <span className="font-normal">{data.artifactType}</span>
             </p>
             <p>
-              Historical Context:{" "}
-              <span className="font-normal">{data.historicalContext}</span>
+              Historical Context: <span className="font-normal">{data.historicalContext}</span>
             </p>
             <p>
               Location: <span className="font-normal">{data.location}</span>
